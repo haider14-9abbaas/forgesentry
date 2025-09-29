c8f0ce6b04c30a18b56b5f16cd713818cdb45eb7
Hamza-hani <nno448512@gmail.com>
2025-09-29 22:08:37 +0500

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  base: "/ourportfolio",
-});
+  optimizeDeps: { exclude: ['lucide-react'] },
+  base: '/', // ← important for Netlify
+})
